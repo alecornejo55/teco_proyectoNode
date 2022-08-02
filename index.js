b5d3ca8197480c74e07636affcb88e8dc0c20aa1
Alex <alex.isaias.cornejo@gmail.com>
2022-08-02 09:37:43 -0300

    Actualización en puerto de escucha

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = 8080;
+const { PORT } = require('./config/globals');
 global.ADMIN = true;
 // MONGODB: mongo. FIRESTORE: firestore
 
--- src/config/globals.js
+++ src/config/globals.js
@@ -4,4 +4,5 @@ module.exports = {
 	MONGO_URI: process.env.MONGO_URI || '',
 	FIRESTORE_FILE: process.env.FIRESTORE_FILE || '',
 	MOTOR: process.env.MOTOR || 'mongo',
+	PORT: process.env.PORT ? process.env.PORT : argv.port ? argv.port : 8080,
 }
